039214e06357fcd4c929ea4eb2e8bb36662385b3
dnjsqhwo <dnjsqhwo@gmail.com>
2022-06-27 09:34:52 +0900

    chore: storybook 설정 추가

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-actions',
   ],
   framework: '@storybook/react',
   core: {
